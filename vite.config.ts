6a0717f81675fd1419e25cf8feb53ea53a271c51
DonCorleone6485 <129817235+DonCorleone6485@users.noreply.github.com>
2026-04-15 19:23:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => { 
-  bsde:'/Simple-Trade/'
+  base:'/Simple-Trade/'
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
