65b5fa0eb979c6c60545a87f3cd851450f2bf8f3
DonCorleone6485 <129817235+DonCorleone6485@users.noreply.github.com>
2026-05-04 07:58:20 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? 'https://obaqhbfaeejepocsdgiv.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iYXFoYmZhZWVqZXBvY3NkZ2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjQyMjgsImV4cCI6MjA5MzQ0MDIyOH0.D8GFUOAKOrIkr0vUKCFpTEDNFCNehq0MNskukIWY2Qg';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://obaqhbfaeejepocsdgiv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iYXFoYmZhZWVqZXBvY3NkZ2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjQyMjgsImV4cCI6MjA5MzQ0MDIyOH0.D8GFUOAKOrIkr0vUKCFpTEDNFCNehq0MNskukIWY2Qg'
+);
